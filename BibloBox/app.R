33be446062eb5fb95e897b23aef48948e95273b3
poppy-nicolette <pnriddle@dal.ca>
2022-10-31 07:00:49 -0300

    woot! its fixed!
    Save this as a version - it works!

--- BibloBox/app.R
+++ BibloBox/app.R
@@ -282,52 +282,49 @@ server <- function(input, output, session) {
   #create and modify dataframe-------------------------------------------------------
   rawData <- eventReactive(input$file1, {
     req(input$file1)
-    #bring in file input
-    df <- read_excel(input$file1$filepath, sheet = 2)
-    template <- df %>% 
+    #template <- read.csv(input$file1$datapath)
+   
+    template <- read_excel(input$file1$datapath, sheet = 2) %>% 
+      mutate(doi = str_sub(doi, str_locate(doi,"10.")[,1])) %>% 
+      mutate(openalex_id = str_sub(openalex_id, str_locate(openalex_id,"W")[,1]))
+    
+    for (i in 1:nrow(template)) {
       
-      # Calculate everything here!
-          mutate(doi = str_sub(doi, str_locate(doi,"10.")[,1])) %>% 
-          mutate(openalex_id = str_sub(openalex_id, str_locate(openalex_id,"W")[,1]))
-        
-        for (i in 1:nrow(template)) {
-          
-          doi<-""
-          id<-""
-          doi<-template[i,]$doi[!is.na(template[i,]$doi)]
-          id<-template[i,]$openalex_id[!is.na(template[i,]$openalex_id)]
-          
-          data <- tibble()
-          data_doi <- tibble()
-          data_id <- tibble()
-          
-          if(length(doi)>0) { 
-            data <- oa_fetch(doi = doi,entity = "works", verbose = TRUE, abstract = TRUE)
-          } else if (length(id)>0) {
-            data <- oa_fetch(identifier = id, entity = "works", verbose = TRUE, abstract = TRUE)
-          }
-          
-          if(length(data)>0){
-            template[i,]$doi <- data$doi
-            template[i,]$openalex_id <- data$id
-            template[i,]$pub_year <- data$publication_year
-            template[i,]$title <- data$display_name
-            template[i,]$authors <- paste(shQuote(data$author[[1]]$au_display_name), collapse=", ")
-            template[i,]$openalex_author_ids <- paste(shQuote(data$author[[1]]$au_id), collapse=", ")
-            template[i,]$source <- data$so
-            template[i,]$volume <- data$volume
-            template[i,]$number <- data$issue
-            if(data$first_page == data$last_page) {pages <- data$first_page} else {pages <- paste(data$first_page,"-",data$last_page, sep = "")}
-            template[i,]$pages <- pages
-            template[i,]$institutions <- paste(shQuote(unique(data$author[[1]]$institution_display_name)), collapse=", ")
-            template[i,]$openalex_institution_ids <- paste(shQuote(unique(data$author[[1]]$institution_id)), collapse=", ")
-            template[i,]$countries <- paste(shQuote(unique(data$author[[1]]$institution_country_code)), collapse=", ")
-            template[i,]$authors_affiliations <- paste(shQuote(paste(data$author[[1]]$au_id," [",data$author[[1]]$institution_id,"]",sep="")), collapse=", ")
-            template[i,]$abstract <- data$ab
-            template[i,]$wikidata_concepts <- paste(shQuote(data$concepts[[1]]$display_name), collapse=", ")
-          }
-        }#close for loop
-        df
+      doi<-""
+      id<-""
+      doi<-template[i,]$doi[!is.na(template[i,]$doi)]
+      id<-template[i,]$openalex_id[!is.na(template[i,]$openalex_id)]
+      
+      data <- tibble()
+      data_doi <- tibble()
+      data_id <- tibble()
+      
+      if(length(doi)>0) { 
+        data <- oa_fetch(doi = doi,entity = "works", verbose = TRUE, abstract = TRUE)
+      } else if (length(id)>0) {
+        data <- oa_fetch(identifier = id, entity = "works", verbose = TRUE, abstract = TRUE)
+      }
+      
+      if(length(data)>0){
+        template[i,]$doi <- data$doi
+        template[i,]$openalex_id <- data$id
+        template[i,]$pub_year <- data$publication_year
+        template[i,]$title <- data$display_name
+        template[i,]$authors <- paste(shQuote(data$author[[1]]$au_display_name), collapse=", ")
+        template[i,]$openalex_author_ids <- paste(shQuote(data$author[[1]]$au_id), collapse=", ")
+        template[i,]$source <- data$so
+        template[i,]$volume <- data$volume
+        template[i,]$number <- data$issue
+        if(data$first_page == data$last_page) {pages <- data$first_page} else {pages <- paste(data$first_page,"-",data$last_page, sep = "")}
+        template[i,]$pages <- pages
+        template[i,]$institutions <- paste(shQuote(unique(data$author[[1]]$institution_display_name)), collapse=", ")
+        template[i,]$openalex_institution_ids <- paste(shQuote(unique(data$author[[1]]$institution_id)), collapse=", ")
+        template[i,]$countries <- paste(shQuote(unique(data$author[[1]]$institution_country_code)), collapse=", ")
+        template[i,]$authors_affiliations <- paste(shQuote(paste(data$author[[1]]$au_id," [",data$author[[1]]$institution_id,"]",sep="")), collapse=", ")
+        template[i,]$abstract <- data$ab
+        template[i,]$wikidata_concepts <- paste(shQuote(data$concepts[[1]]$display_name), collapse=", ")
+      }
+    }
       #-**************************************************
       }
     )#close eventReactive
@@ -339,7 +336,7 @@ rawData
     filename = function() {paste("modified template_",  Sys.Date(), input$downloadType)},
     content = function(file){
       if(input$downloadType == ".csv") {
-        write.csv(rawData(), file1, row.names = FALSE)}
+        write.csv(template, file, row.names = FALSE)}
       
       else if(input$downloadType == ".xlsx") {
         

deb8435c4fef13e3f1c4ae7f556893add86fb687
poppy <75341794+poppy-nicolette@users.noreply.github.com>
2025-04-08 16:43:41 -0300

    changed map page

--- dashboard-tips/app.py
+++ dashboard-tips/app.py
@@ -6,6 +6,7 @@ import pandas as pd
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 
 
+
 #load data for table
 df=pd.read_csv("www/org_locations_ns.csv")
 
@@ -15,18 +16,63 @@ df=pd.read_csv("www/org_locations_ns.csv")
     - table for literature table
     - tab for demographics
 """
-app_ui = ui.page_fluid( 
-                ui.panel_well(
-                ui.panel_title("hello there"),
-                ui.card("card 1",
-                    output_widget("map")),
-                ui.card(
-                    ui.card_header("Data Frame as ", ui.tags.code("render.DataTable")),
-                    "Table of education organizations in Nova Scotia",
-                    ui.output_data_frame("table"),
-                        ),
-        ),
-        )
+app_ui = ui.page_navbar(
+    ui.nav_spacer(),
+    ui.nav_panel(
+        "Main Page info",
+        ui.layout_sidebar(
+            ui.sidebar(
+                "some placeholder text",
+                position="right",
+                width=300,
+                title="some text here too"),
+            ),#close sidebar
+            ui.card("Some more room if you need it"),
+        ),#close layout_sidebar
+
+    # Map of resources nav_panel
+    ui.nav_panel(
+        "Map of resources",
+        ui.layout_columns(
+            ui.card("Below is a map of education centres in Nova Scotia. Hover over a marker to bring up more infomation on the centre. Names and address of each centre can be found in the table below."
+                ,height="100px"),
+            col_widths=[12],
+            min_height="120px",
+            max_height="auto"
+        ),#close layout_columns
+        ui.layout_columns(
+            ui.card(
+                output_widget("map")),
+            ui.card(
+                "Table of education organizations in Nova Scotia",
+                ui.output_data_frame("table"),),
+            col_widths=[8,4],
+            min_height="600px",
+            max_height="800px"
+        ),#close layout_columns
+        ui.layout_columns(
+            
+                col_widths=[-2,8,-2],
+                min_height="600px",
+                max_height="800px"
+        ),#close layout_columns
+    ),#close nav_panel
+
+    #Title bar at top
+    fillable="Main Page info",
+    id="navbar",
+    title=ui.popover(
+        ["Literacy Nova Scotia Dashboard"],
+        ui.markdown("This project aims to make results from literacy studies available to those in the LNS community."),
+        placement="right",
+    ),
+    window_title="Literacy NS Dashboard",
+
+
+)#close page_navbar
+
+#output_widget("map")
+
 
 def server(input, output, session):
     @render_widget

45ea6a1ffdbaee0142e28a016f0d393fd19f3dc7
poppy <75341794+poppy-nicolette@users.noreply.github.com>
2025-04-08 11:31:10 -0300

    added map and table

--- dashboard-tips/app.py
+++ dashboard-tips/app.py
@@ -1,16 +1,30 @@
-from shiny import App, ui
+
 import plotly.express as px
 import plotly.graph_objects as go
 from shinywidgets import output_widget, render_widget
 import pandas as pd
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 
 
+#load data for table
+df=pd.read_csv("www/org_locations_ns.csv")
+
+"""
+ - add tabs for each thing
+    - tab for map of organizations
+    - table for literature table
+    - tab for demographics
+"""
 app_ui = ui.page_fluid( 
                 ui.panel_well(
                 ui.panel_title("hello there"),
                 ui.card("card 1",
-                output_widget("map")),
-                ui.card("card 2"),
+                    output_widget("map")),
+                ui.card(
+                    ui.card_header("Data Frame as ", ui.tags.code("render.DataTable")),
+                    "Table of education organizations in Nova Scotia",
+                    ui.output_data_frame("table"),
+                        ),
         ),
         )
 
@@ -27,11 +41,26 @@ def server(input, output, session):
         site_lng = df.lng
         locations = df.organization
         address = df.address
-        fig = px.scatter_map(df,lat=site_lat, lon=site_lng, zoom=6, hover_data=[locations,address])
+        fig = px.scatter_map(df,
+                            lat=site_lat,
+                            lon=site_lng,
+                            zoom=6,
+                            hover_data=[locations,address],
+                            map_style='open-street-map',
+                            subtitle="copyright OpenStreetMaps")
         fig.update_layout(
             title="A map of organizations in NS",
             hovermode='closest',
         )
         return fig
+
+    @render.data_frame
+    def table():
+        return render.DataTable(
+            data=df,
+            filters=False,
+            editable=False,
+            #selection_mode=input.selection_mode(),
+        )
 
 app = App(app_ui,server)

0576b3a3e64b8b671bb64f184faef53c4feee35d
poppy <75341794+poppy-nicolette@users.noreply.github.com>
2025-04-23 08:58:41 -0300

    dleleted venv

--- dashboard-tips/venv/lib/python3.12/site-packages/shiny/api-examples/input_text/app-express.py
+++
@@ -1,8 +0,0 @@
-from shiny.express import input, render, ui
-
-ui.input_text("caption", "Caption:", "Data summary")
-
-
-@render.code
-def value():
-    return input.caption()
--- dashboard-tips/venv/lib/python3.12/site-packages/shiny/api-examples/panel_title/app-express.py
+++
@@ -1,3 +0,0 @@
-from shiny.express import ui
-
-ui.panel_title("Page title", "Window title")

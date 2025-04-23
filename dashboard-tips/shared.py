178020fb737e540c22c38b4e43d27a859c9eb672
poppy <75341794+poppy-nicolette@users.noreply.github.com>
2025-04-23 08:47:37 -0300

    cleanup

--- dashboard-tips/shared.py
+++ dashboard-tips/shared.py
@@ -3,4 +3,4 @@ from pathlib import Path
 import pandas as pd
 
 app_dir = Path(__file__).parent
-tips = pd.read_csv(app_dir / "tips.csv")
+tips = pd.read_csv(app_dir /www/ "LNS_openalex_full_metadata_REV2.csv")
